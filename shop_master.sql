2fa639b327c0278c299d2f0fa558795b1e4c6dc2
mownikak-cs21 <mownikak.cs21@kascsathy.ac.in>
2023-08-15 12:15:40 +0000

    first commit

--- shop_master.sql
+++ shop_master.sql
@@ -1,12 +1,12 @@
-insert into skfp.shop_master(shop_id,shop_name,address_line1,address_line2,address_line3,pincode,contact_no)values('','Anbu maligai','DG puthur','gobi','erode','638503','7689655643');
-insert into skfp.shop_master(shop_id,shop_name,address_line1,address_line2,address_line3,pincode,contact_no)values('','SPAS bakery','sakthi nagar','anthiyur','erode','638314','7698543498');
-insert into skfp.shop_master(shop_id,shop_name,address_line1,address_line2,address_line3,pincode,contact_no)values('','MPS bakery','gandhi nagar','bhavani','erode','638312','8976545467');
+insert into skfp.shop_master(shop_name,address_line1,address_line2,address_line3,pincode,contact_no,created_by,created_date,last_updated_by,last_updated_date,status)values('Anbu maligai','DG puthur','gobi','erode','638503','7689655643','mownika',NOW(),'mownika',NOW(),'1');
+insert into skfp.shop_master(shop_name,address_line1,address_line2,address_line3,pincode,contact_no,created_by,created_date,last_updated_by,last_updated_date,status)values('SPAS bakery','sakthi nagar','anthiyur','erode','638314','7698543498','mownika',NOW(),'mownika',NOW(),'1');
+insert into skfp.shop_master(shop_name,address_line1,address_line2,address_line3,pincode,contact_no,created_by,created_date,last_updated_by,last_updated_date,status)values('MPS bakery','gandhi nagar','bhavani','erode','638312','8976545467','mownika',NOW(),'mownika',NOW(),'1');
 
-insert into skfp.shop_master(shop_id,shop_name,address_line1,address_line2,address_line3,pincode,contact_no)values('','kasturi bakes','kugalur','gobi','erode','635087','8790654567');
+insert into skfp.shop_master(shop_name,address_line1,address_line2,address_line3,pincode,contact_no,created_by,created_date,last_updated_by,last_updated_date,status)values('kasturi bakes','kugalur','gobi','erode','635087','8790654567','mownika',NOW(),'mownika',NOW(),'1');
 
-insert into skfp.shop_master(shop_id,shop_name,address_line1,address_line2,address_line3,pincode,contact_no)values('','Indian store','appakudal','bhavani','erode','638315','9878654576');>
+insert into skfp.shop_master(shop_name,address_line1,address_line2,address_line3,pincode,contact_no,created_by,created_date,last_updated_by,last_updated_date,status)values('Indian store','appakudal','bhavani','erode','638315','9878654576','mownika',NOW(),'mownika',NOW(),'1');
 
-insert into skfp.shop_master(shop_id,shop_name,address_line1,address_line2,address_line3,pincode,contact_no)values('','Kutty bakery','athani','anthiyur','erode','638315','7865439876'); >
+insert into skfp.shop_master(shop_name,address_line1,address_line2,address_line3,pincode,contact_no,created_by,created_date,last_updated_by,last_updated_date,status)values('Kutty bakery','athani','anthiyur','erode','638315','7865439876','mownika',NOW(),'mownika',NOW(),'1'); 
 
 
 
